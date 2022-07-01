28c13e39f36b086a81a6bd27598f696bd4bcc10f
instrovate <lms@instrovate.com>
2022-07-01 13:25:53 +0530

    first

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 const auth = require('./middleware/auth');
+const cookieParser = require('cookie-parser');
+ 
+
+
+// Stripe routes
+const striperoutes = require('./routes/stripe-route');
+
 
 // routes
+const requests = require('./routes/api/Req');
+const applications = require('./routes/api/Application');
 const books = require('./routes/api/books');
+
 const users = require('./routes/api/Users');
 const Professionals = require('./routes/api/Professionals');
 
@@ -14,45 +24,60 @@ const app = express();
 
 // Connect Database
 connectDB();
-
+app.use(cookieParser());
 // cors
-app.use(cors({ origin: true, credentials: true }));
+//app.use(cors({ origin: true, credentials: true }));
+app.use( (req, response, next)=> {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+response.setHeader("Access-Control-Allow-Credentials", "true");
+response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+  next();
+});
+app.use(cors());
+
+
+
+/*
+app.get('/setcookie', (req, res) => {
+  res.cookie('email','test');
+  res.send('Cookie have been saved successfully');
+});
+
+*/
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
 //app.get('/', (req, res) => res.send('Hello world!'));
 
-if(process.env.NODE_ENV == "production"){
-  app.use(express.static("./client/build"));
+
+if ( process.env.NODE_ENV == "production"){
+
+  app.use(express.static("react-frontend/build"));
+
   const path = require("path");
-  app.get("*", (req, res) => {`enter code here`
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+  app.get("*", (req, res) => {
+
+      res.sendFile(path.resolve(__dirname, 'react-frontend', 'build', 'index.html'));
 
   })
+
+
 }
 
-app.get('/', (request, response) => {
 
-  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  
-   });
-
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
 
 // use Routes
 app.use('/api/books', books);
 app.use('/api/users', users);
 app.use('/api/professionals', Professionals);
+app.use('/api/requests', requests);
+app.use('/api/applications', applications);
+app.use('/api/stripe', striperoutes);
+
 const port = process.env.PORT || 8082;
-
-
-
-
-
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
